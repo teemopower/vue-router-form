d7843449af11f928c4a9bd106c83464529c64350
teemopower <jimmykim925@gmail.com>
2020-01-01 04:23:37 -0800

    updated props

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -3,7 +3,7 @@
     <br>
     <br>SAMPLE COMPONENT
     <br>Here is the prop data ---
-    <div v-for="(test, index) in testProp">{{index}}. {{ test }}</div>
+    <div v-for="(test, index) in testProp" :key="index">{{index}}. {{ test }}</div>
   </div>
 </template>
 
@@ -14,7 +14,14 @@ export default {
     return {};
   },
   props: ["testProp"],
-  methods: {}
+  methods: {
+    accessPropData() {
+      console.log(this.testProp);
+    }
+  },
+  mounted() {
+    this.accessPropData();
+  }
 };
 </script>
 
